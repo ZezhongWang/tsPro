e22089316d8c1b69cf54a3335a006655f76bb082
wangzezhong <904192625@qq.com>
2018-01-21 21:57:37 -0500

    Build processor architechture

--- dataProcessor.py
+++ dataProcessor.py
@@ -1,4 +1,11 @@
+from processor.drawLineChart import draw_line_chart
+from processor.calcCorr import calc_corr
+
 class DataProcessor(object):
 
     def run(self):
-        pass
+        calc_corr()
+        draw_line_chart()
+
+
+    
